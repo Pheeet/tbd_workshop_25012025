cef58cd2bec9d8ff579515f88b9d3b805f565b7c
tin-auppati <tin2548120@gmail.com>
2025-01-25 15:05:45 +0700

    add hi

--- Exercise/script.js
+++ Exercise/script.js
@@ -8,6 +8,7 @@ function say_hello() {
     // CODE HERE!!!
     console.log("What is your name?")
     console.log("My name is Tin auppati")
+    console.log("Hi muda")
 }
 
 // Don’t change the code under this comment!
