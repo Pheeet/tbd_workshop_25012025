662a5db530e3c3a3ae4dbdc269f7fe05d4041ebf
tin-auppati <tin2548120@gmail.com>
2025-01-25 14:56:40 +0700

    add name exercise

--- Exercise/script.js
+++ Exercise/script.js
@@ -7,7 +7,7 @@
 function say_hello() {
     // CODE HERE!!!
     console.log("What is your name?")
-    
+    console.log("My name is Tin auppati")
 }
 
 // Don’t change the code under this comment!
